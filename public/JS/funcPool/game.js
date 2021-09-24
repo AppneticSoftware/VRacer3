804b224398b230a309e73dfdb79c771c01731902
unknown <maxim.bickel@appnetic.de>
2021-09-24 08:44:22 +0200

    TestingVRButton

--- public/JS/funcPool/game.js
+++ public/JS/funcPool/game.js
@@ -1,34 +1,109 @@
-import { THREE } from "../mainProgramm.js";
+import * as THREE from "../../libs/three/three.module.js";
+import { GLTFLoader } from "../../libs/three/jsm/GLTFLoader.js";
+import { OrbitControls } from "../../libs/three/jsm/OrbitControls.js";
+import { LoadingBar } from "../../libs/LoadingBar.js";
+import { VRButton } from "../../libs/three/jsm/VRButton.js";
 
-export function startGame() {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  console.log("Three");
+class Game {
+  constructor() {
+    const container = document.createElement("div");
+    document.body.appendChild(container);
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
-  document.body.appendChild(renderer.domElement);
+    this.camera = new THREE.PerspectiveCamera(
+      60,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      100
+    );
 
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+    this.camera.position.set(0, 1, 10);
 
-  camera.position.z = 5;
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xaaaaaa);
 
-  const animate = function () {
-    requestAnimationFrame(animate);
+    const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.5);
+    this.scene.add(ambient);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    const light = new THREE.DirectionalLight(0xffffff, 1.5);
+    light.position.set(0.2, 1, 1);
+    this.scene.add(light);
 
-    renderer.render(scene, camera);
-  };
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+    this.renderer.physicallyCorrectLights = true;
+    container.appendChild(this.renderer.domElement);
 
-  animate();
+    this.loadingBar = new LoadingBar();
+
+    this.loadGLTF();
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.target.set(0, 0, 0);
+    this.controls.update();
+
+    window.addEventListener("resize", this.resize.bind(this));
+  }
+
+  loadGLTF() {
+    const loader = new GLTFLoader().setPath("../../assets/");
+    const self = this;
+
+    // Load a glTF resource
+    loader.load(
+      // resource URL
+      "Car.glb",
+      // called when the resource is loaded
+      function (gltf) {
+        const bbox = new THREE.Box3().setFromObject(gltf.scene);
+        console.log(
+          `min:${bbox.min.x.toFixed(2)},${bbox.min.y.toFixed(
+            2
+          )},${bbox.min.z.toFixed(2)} -  max:${bbox.max.x.toFixed(
+            2
+          )},${bbox.max.y.toFixed(2)},${bbox.max.z.toFixed(2)}`
+        );
+
+        gltf.scene.traverse((child) => {
+          if (child.isMesh) {
+            child.material.metalness = 0.2;
+          }
+        });
+        self.chair = gltf.scene;
+        self.chair.scale.set(5, 5, 5);
+        self.scene.add(gltf.scene);
+
+        self.loadingBar.visible = false;
+
+        self.renderer.setAnimationLoop(self.render.bind(self));
+      },
+      // called while loading is progressing
+      function (xhr) {
+        self.loadingBar.progress = xhr.loaded / xhr.total;
+      },
+      // called when loading has errors
+      function (error) {
+        console.log("An error happened");
+      }
+    );
+  }
+
+  resize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  render() {
+    this.chair.rotateY(0.01);
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  setupVRButton() {
+    this.renderer.xr.enabled = true;
+    document.body.appendChild(VRButton.createButton(this.renderer));
+  }
 }
+
+export { Game };
--- public/JS/mainProgramm.js
+++ public/JS/mainProgramm.js
@@ -1,10 +1,12 @@
-import * as THREE from "../libThree/three.module.js";
-export * as THREE from "../libThree/three.module.js";
+// import * as THREE from "../libThree/three.module.js";
+// export * as THREE from "../libThree/three.module.js";
 
-import { startGame } from "./funcPool/game.js";
+import { Game } from "./funcPool/game.js";
 import { startCommunication } from "./funcPool/communication.js";
 
 window.onload = function () {
-  startGame();
-  startCommunication();
+  const game = new Game();
+  game.setupVRButton();
+  window.app = game;
+  // startCommunication();
 };
