3829e35f0a484ebb2161e7a17f2a553246b1e8f7
unknown <accounts@appnetic.de>
2021-10-14 11:21:21 +0200

    Update game.js

--- public/JS/funcPool/game.js
+++ public/JS/funcPool/game.js
@@ -231,7 +231,7 @@ class Game {
     } else {
       this.raceDolly.children[0].rotation.z = 0;
       this.raceDolly.children[1].rotation.z = 0;
-      this.raceDolly.children[2].rotation.z = 0;
+      this.raceDolly.children[2].children[2].rotation.z = 0;
       this.raceDolly.position.y = 0;
     }
   }
@@ -331,7 +331,8 @@ class Game {
     ) {
       this.raceDolly.children[0].rotation.z -= this.maxRotation * this.xStick;
       this.raceDolly.children[1].rotation.z += this.maxRotation * this.xStick;
-      this.raceDolly.children[2].rotation.z += this.maxRotation * this.xStick;
+      this.raceDolly.children[2].children[2].rotation.z +=
+        this.maxRotation * this.xStick;
     }
     this.raceDolly.position.y = 0;
   }
