c8e9d3d9a4958c860bbd413bced03c82000bdf21
unknown <accounts@appnetic.de>
2021-10-19 16:21:02 +0200

    Update game.js

--- public/JS/funcPool/game.js
+++ public/JS/funcPool/game.js
@@ -240,6 +240,8 @@ class Game {
     if (this.stickButton != 0) {
       //WEBXR SECOND TEST BTN
       // this.main.communication.sendUserVoteStartGame();
+      this.manageExitButtonPressed();
+
       //Show UI;
       this.manageUI_Visibility();
     }
